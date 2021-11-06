bf22c784ae8f5c36b22221698f645d35f7322e33
winfried <ozgurarslln@icloud.com>
2021-11-06 23:11:32 +0300

    beta

--- powershell/automatic-opensusetw.ps1
+++ powershell/automatic-opensusetw.ps1
@@ -1,9 +1,5 @@
 if (!([Security.Principal.WindowsPrincipal][Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole] "Administrator")) { Start-Process powershell.exe "-NoProfile -ExecutionPolicy Bypass -File `"$PSCommandPath`"" -Verb RunAs; exit }
-param(
-[bool]$wsa,
-[bool]$gapps,
-[bool]$vmc
-)
+param( [bool]$wsa, [bool]$gapps, [bool]$vmc )
 $Arch = (Get-Process -Id $PID).StartInfo.EnvironmentVariables["PROCESSOR_ARCHITECTURE"];
 if ($Arch -eq 'x86') {
     Write-Host -Object 'Running 32-bit PowerShell';
