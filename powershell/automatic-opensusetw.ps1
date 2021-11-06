d53bde86b0b69b88d9daff756bb18b94e3c08d25
winfried <ozgurarslln@icloud.com>
2021-11-06 23:09:05 +0300

    beta

--- powershell/automatic-opensusetw.ps1
+++ powershell/automatic-opensusetw.ps1
@@ -1,5 +1,4 @@
 if (!([Security.Principal.WindowsPrincipal][Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole] "Administrator")) { Start-Process powershell.exe "-NoProfile -ExecutionPolicy Bypass -File `"$PSCommandPath`"" -Verb RunAs; exit }
-$vmc = $true
 param(
 [bool]$wsa,
 [bool]$gapps,
