6c8bcb4d84586ed57f9abce10a13427fb86e47ca
winfried <ozgurarslln@icloud.com>
2021-12-08 23:22:52 +0300

    Fixes, Improvements

--- powershell/wsadevelopermode.ps1
+++ powershell/wsadevelopermode.ps1
@@ -1,3 +1,9 @@
+if (!([Security.Principal.WindowsPrincipal][Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole] "Administrator")) { Start-Process powershell.exe "-ExecutionPolicy Bypass `"$PSCommandPath`" -nostop $nostopint" -Verb RunAs; exit }
+
+
+reg add "HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows\CurrentVersion\AppModelUnlock" /t REG_DWORD /f /v "AllowDevelopmentWithoutDevLicense" /d "1"
+
+
 $wsalocation = "$env:LOCALAPPDATA/Packages/MicrosoftCorporationII.WindowsSubsystemForAndroid_8wekyb3d8bbwe/Settings/settings.dat"
               $mountPoint = "HKLM\WSA"
 reg load $mountPoint $wsalocation
