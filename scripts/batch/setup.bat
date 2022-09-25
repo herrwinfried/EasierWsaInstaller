517b9257af82c34248896d99b9bbd269689f4aef
Winfried <ozgurarslln@icloud.com>
2022-09-25 15:27:14 +0300

    V2.0.1 Beta
    Project still under development Fixed some bugs.

---
+++ scripts/batch/setup.bat
@@ -0,0 +1,1 @@
+PowerShell -NoProfile -ExecutionPolicy Bypass -Command "Start-Process PowerShell '-NoProfile -ExecutionPolicy Bypass -File \"C:\Public\File\SomeScript.ps1\" Arg1 \"%1\"' -Verb RunAs"
