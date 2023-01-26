17315de628ce2c1e6da05074ebfa92acc198dfe0
winfried <winfried@winfried>
2023-01-26 21:43:40 +0300

    Trial 1

---
+++ src/tools/README.md
@@ -0,0 +1,3 @@
+To keep updated :)
+
+_The most valuable thing given to us for free is time... :p_ 
--- src/tools/Update-Wsagascript.sh
+++ src/tools/Update-Wsagascript.sh
@@ -1,1 +1,23 @@
-https://github.com/WSA-Community/WSAGAScript.git
+ScriptLocal=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+if [[ $EUID -eq 0 ]]; then
+     echo $"$red You need to be not Super User/Root. $white"
+   exit 1
+fi
+if ! [ -x "$(command -v git)" ]; then
+echo $"$green I found a missing package, I'm installing it... (git) $white"
+if [ -x "$(command -v apt)" ]; then
+sudo apt install -y git
+elif [ -x "$(command -v zypper)" ]; then
+sudo zypper in -y -l git
+fi
+fi
+cd $ScriptLocal
+cd ..
+if [ -d WSAGAScript ]; then
+rm -rf WSAGAScript
+fi
+git clone https://github.com/WSA-Community/WSAGAScript.git
+
+cd WSAGAScript
+rm -rf .git
+
