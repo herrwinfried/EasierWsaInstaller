f80b827eed8b6cafa2b098d10da971f982f14404
Winfried <ozgurarslln@icloud.com>
2023-01-28 22:37:35 +0300

    Output Edit

--- src/EasierWsaInstaller/guiSupport/requirements.ps1
+++ src/EasierWsaInstaller/guiSupport/requirements.ps1
@@ -48,9 +48,9 @@ write-Host "TR: LUTFEN DIKKAT" -ForegroundColor white -Background red
 write-Host "EN: If you want to delete Ubuntu, powershell core and WSL, use this command: " -ForegroundColor yellow
 write-Host "TR: Ubuntu, powershell core ve WSL'yi silmek istiyorsaniz su komutu kullanin:" -ForegroundColor white -Background red
 Write-Host " "
-write-Host "winget.exe install --id 9P9TQF7MRM4R"
-write-Host "winget.exe install --id 9PDXGNCFSCZV"
-write-Host "winget.exe install --id 9MZ1SNWT0N5D"
+write-Host "winget.exe uninstall --id 9P9TQF7MRM4R"
+write-Host "winget.exe uninstall --id 9PDXGNCFSCZV"
+write-Host "winget.exe uninstall --id 9MZ1SNWT0N5D"
 Start-Sleep -s 3
 write-Host "EN: If you want to turn off the virtual machine platform, you can turn it off if you type 'Turn Windows features on or off' in the search. " -ForegroundColor yellow
 write-Host "TR: Sanal makine platformunu kapatmak istiyorsaniz arama kismina 'Windows ozelliklerini ac veya kapat' yazarsaniz kapatabilirsiniz." -ForegroundColor white -Background red
