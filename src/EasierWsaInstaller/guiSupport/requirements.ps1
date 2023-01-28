072136cf05809768b3ad9df507a0da2f98ef0d4f
Winfried <ozgurarslln@icloud.com>
2023-01-28 17:33:49 +0300

    Fix

--- src/EasierWsaInstaller/guiSupport/requirements.ps1
+++ src/EasierWsaInstaller/guiSupport/requirements.ps1
@@ -27,7 +27,7 @@ $vmp += "-c "
 $vmp += "dism.exe /online /enable-feature /featurename:VirtualMachinePlatform /all /norestart"
 $vmp += "'"
 write-Host "EN: You will need administrator permission to enable the virtual machine platform..." -ForegroundColor yellow
-write-Host "TR: Sanal makine platformunu etkinleştirmek için yönetici iznine ihtiyacınız olacak..." -ForegroundColor white -Background red
+write-Host "TR: Sanal makine platformunu etkinlestirmek icin yönetici iznine ihtiyaciniz olacak..." -ForegroundColor white -Background red
 Start-Sleep -s 7
 Invoke-Expression $vmp
 
@@ -44,20 +44,20 @@ wingetx -Id 9PDXGNCFSCZV
 wingetx -Id 9MZ1SNWT0N5D
 
 write-Host "EN: Attention please" -ForegroundColor red
-write-Host "TR: LÜTFEN DİKKAT" -ForegroundColor white -Background red
+write-Host "TR: LUTFEN DIKKAT" -ForegroundColor white -Background red
 write-Host "EN: If you want to delete Ubuntu, powershell core and WSL, use this command: " -ForegroundColor yellow
-write-Host "TR: Ubuntu, powershell core ve WSL'yi silmek istiyorsanız şu komutu kullanın:" -ForegroundColor white -Background red
+write-Host "TR: Ubuntu, powershell core ve WSL'yi silmek istiyorsaniz su komutu kullanin:" -ForegroundColor white -Background red
 Write-Host " "
 write-Host "winget.exe install --id 9P9TQF7MRM4R"
 write-Host "winget.exe install --id 9PDXGNCFSCZV"
 write-Host "winget.exe install --id 9MZ1SNWT0N5D"
 Start-Sleep -s 3
 write-Host "EN: If you want to turn off the virtual machine platform, you can turn it off if you type 'Turn Windows features on or off' in the search. " -ForegroundColor yellow
-write-Host "TR: Sanal makine platformunu kapatmak istiyorsanız arama kısmına 'Windows özelliklerini aç veya kapat' yazarsanız kapatabilirsiniz." -ForegroundColor white -Background red
+write-Host "TR: Sanal makine platformunu kapatmak istiyorsaniz arama kismina 'Windows ozelliklerini ac veya kapat' yazarsaniz kapatabilirsiniz." -ForegroundColor white -Background red
 Write-Host " "
 Start-Sleep -s 3
 write-Host "EN: Don't forget to restart your computer! " -ForegroundColor yellow
-write-Host "TR: Bilgisayarınızı yeniden başlatmayı unutmayın!" -ForegroundColor white -Background red
+write-Host "TR: Bilgisayarinizi yeniden baslatmayi unutmayin!" -ForegroundColor white -Background red
 Write-Host " "
 Start-Sleep -s 2
 write-Host "EN: After rebooting, open the application named Ubuntu And perform the Installation. " -ForegroundColor yellow
